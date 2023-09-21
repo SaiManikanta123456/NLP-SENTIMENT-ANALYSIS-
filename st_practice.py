d4bbccee0e6f0ebdc28a5d35a9d299bc2eb6a52a
L.Sai Manikanta <113752452+SaiManikanta123456@users.noreply.github.com>
2023-09-21 10:55:12 +0530

    Update st_practice.py

--- st_practice.py
+++ st_practice.py
@@ -27,7 +27,7 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-add_bg_from_local("D:/SIXTH SEMISTER/bg_blur.jpg")
+# add_bg_from_local("D:/SIXTH SEMISTER/bg_blur.jpg")
 
 
 
